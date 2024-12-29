52ea93dfdf765e7d3c17e585b0f00c23cf16fced
[Sumit] <[sumitadhikari166@gmail.com]>
2024-12-29 18:47:57 +0530

    change in Navbar.jsx

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div>
           <h1 className="text-3xl font-bold">
-            Job<span className="text-[#F83002]">Portal</span>
+            Job<span className="text-[#F83002]">zzY</span>
           </h1>
         </div>
 
